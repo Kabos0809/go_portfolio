2998ceceaf9321047e09b1739a1984626083a0c1
Kabos0809 <hiyamazu70@gmail.com>
2022-09-26 00:11:11 +0900

    initial commit

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,9 @@ module github.com/kabos0809/go_portfolio/backend
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,10 @@ import (
     "fmt"
     "log"
     "net/http"
+	"github.com/jinzhu/gorm"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
     "github.com/gorilla/mux"
 )
 
@@ -13,10 +16,26 @@ func main() {
     router := mux.NewRouter().StrictSlash(true)
     // 2. URLと処理を紐付ける
     router.HandleFunc("/", home)
+	router.HandleFunc("/check_db", check_cnct_db)
     // 3. ポートを指定して起動
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World")
+    fmt.Fprintf(w, "World")
+}
+
+func check_cnct_db(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	database := os.Getenv("MYSQL_DATABASE")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, database)
+	db, err := gorm.Open("mysql", connection)
+
+	defer db.Close()
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 }
